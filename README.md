4f6c79060a46fdcdbf9bb328e8264cd08c4dcc0a
Loic Denuziere <loic.denuziere@intellifactory.com>
2018-09-07 11:53:43 +0200

    Initial commit for JsPDF

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WebSharper @sampleName@
+# WebSharper JsPDF
 
-Sample for [WebSharper](https://websharper.com) bindings for @libLink@.
+Sample for [WebSharper](https://websharper.com) bindings for [JsPDF](https://parall.ax/products/jspdf).
 
-* [Source repository on GitHub](@repoUrl@)
-* [See it running live](@liveUrl@)
+* [Source repository on GitHub](https://github.com/websharper-samples/JsPDF)
+* [See it running live](https://websharper-samples.github.io/JsPDF)
--- src/Client.fs
+++ src/Client.fs
@@ -1,1 +1,37 @@
-TODO Replace me
+namespace WebSharper.JsPDF.Sample
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.JsPDF
+open WebSharper.JQuery
+open WebSharper.UI
+open WebSharper.UI.Client
+open WebSharper.UI.Html
+open WebSharper.UI.Templating
+
+[<JavaScript>]
+module Client =
+    type IndexTemplate = Template<"wwwroot/index.html", ClientLoad.FromDocument>
+
+    [<SPAEntryPoint>]
+    let Main () =
+
+        let doc = new JsPDF()
+
+        doc.Text("Hello world!", 10., 10., obj) |> ignore
+        doc.AddPage()
+        doc.Text("Hello world on Page 2!", 10., 10., obj) |> ignore
+        let pdf = doc.Output("bloburl", obj).ToString()
+
+        let pdfRender =
+            Tags.``object`` [
+                attr.width "100%"
+                attr.height "100%"
+                attr.data pdf
+                attr.``type`` "application/pdf"
+            ] []
+
+        IndexTemplate.Main()
+            .PDF(pdfRender)
+            .Doc()
+        |> Doc.RunById "main"
